a471d53447a54396b8f171a015941fa0d66f9fc4
Cosimo-Colucci <ccoluccilavoro@gmail.com>
2023-06-20 12:00:24 +0200

    istalled axios

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -1,16 +1,25 @@
 <script>
 import CardComponent from './CardComponent.vue';
-import {store} from '../store'
+
+import {store} from '../store.js'
+import axios from 'axios';
 
     export default{
         name: 'MainApp',
         data(){
             return{
+                apiUrl:'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0',
                 store,
             }
         },
         components: {
             CardComponent,
+        },
+        created(){
+             axios.get(this.apiUrl)
+            .then( (response) => {
+                console.log(response);
+            })
         }
     }
 </script>
